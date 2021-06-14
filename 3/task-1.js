1c03e280148b087864baeb5c10ab9bbf6e52de89
oleg-tgn <stelmah.taganrog@gmail.com>
2021-06-14 18:07:09 +0300

    Lesson 3. Tasl-1 Password

---
+++ 3/task-1.js
@@ -0,0 +1,6 @@
+let password = "qaz-xsw"
+
+if (password.length >= 4 && (password.includes('-') || password.includes('_')))
+    console.log("Пароль надёжный");
+else
+    console.log("Пароль недостаточно надёжный");
